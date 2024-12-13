e56a8b166b651aa2cb6578aa92a8391148415e75
my-name-is-samael <warkramzec@gmail.com>
2024-12-13 02:15:30 +0100

    Add core texts formatting hints + preview #44

--- BeamJoyInterface/lua/ge/extensions/BJI/ui/WindowServer/CoreCen.lua
+++ BeamJoyInterface/lua/ge/extensions/BJI/ui/WindowServer/CoreCen.lua
@@ -1,7 +1,158 @@
+local FORMATTING_CODES = {
+    {
+        { code = "^r", key = "reset" },
+        { code = "^n", key = "underline" },
+        { code = "^l", key = "bold" },
+        { code = "^m", key = "strike" },
+        { code = "^o", key = "italic" },
+    },
+    {
+        { code = "^0", key = "black",    color = RGBA(0, 0, 0, 1) },
+        { code = "^1", key = "darkblue", color = RGBA(0, 0, .67, 1) },
+        { code = "^2", key = "green",    color = RGBA(0, .67, 0, 1) },
+        { code = "^3", key = "darkaqua", color = RGBA(0, .67, .67, 1) },
+        { code = "^4", key = "red",      color = RGBA(.67, 0, 0, 1) },
+        { code = "^5", key = "purple",   color = RGBA(.67, 0, .67, 1) },
+        { code = "^6", key = "orange",   color = RGBA(1, .67, 0, 1) },
+        { code = "^7", key = "grey",     color = RGBA(.67, .67, .67, 1) },
+    },
+    {
+        { code = "^8", key = "darkgrey",   color = RGBA(.33, .33, .33, 1) },
+        { code = "^9", key = "blue",       color = RGBA(.33, .33, 1, 1) },
+        { code = "^a", key = "lightgreen", color = RGBA(.33, 1, .33, 1) },
+        { code = "^b", key = "aqua",       color = RGBA(.33, 1, 1, 1) },
+        { code = "^c", key = "lightred",   color = RGBA(1, .33, .33, 1) },
+        { code = "^d", key = "pink",       color = RGBA(1, .33, 1, 1) },
+        { code = "^e", key = "yellow",     color = RGBA(1, 1, .33, 1) },
+        { code = "^f", key = "white",      color = RGBA(1, 1, 1, 1) },
+    }
+}
+local FAVORITE_NAME_OFFLINE_SUFFIX = "[OFFLINE]"
+
+local function drawCoreFormattingHints()
+    local function renderCode(el)
+        LineBuilder()
+            :text(svar("{1} :", { el.code }))
+            :text(BJILang.get(svar("serverConfig.core.formattingHints.{1}", { el.key })), el.color)
+            :build()
+    end
+    AccordionBuilder()
+        :label(BJILang.get("serverConfig.core.formattingHints.title"))
+        :openedBehavior(function()
+            local cols = ColumnsBuilder("FormattingHints", { -1, -1, -1 })
+            local iMax = math.max(#FORMATTING_CODES[1], #FORMATTING_CODES[2], #FORMATTING_CODES[3])
+            for i = 1, iMax do
+                cols:addRow({
+                    cells = {
+                        FORMATTING_CODES[1][i] and function()
+                            renderCode(FORMATTING_CODES[1][i])
+                        end,
+                        FORMATTING_CODES[2][i] and function()
+                            renderCode(FORMATTING_CODES[2][i])
+                        end,
+                        FORMATTING_CODES[3][i] and function()
+                            renderCode(FORMATTING_CODES[3][i])
+                        end,
+                    }
+                })
+            end
+            cols:build()
+        end)
+        :build()
+end
+
+local function drawCoreTextPreview(cols, value, key)
+    local reset = {
+        char = FORMATTING_CODES[1][1].code:sub(2),               -- RESET CHAR
+        color = FORMATTING_CODES[3][#FORMATTING_CODES[3]].color, -- WHITE COLOR
+    }
+    local newlineChar = "p"                                      -- NEWLINE CHAR
+
+    local str = {}
+    local parts = ssplit(value, "^")
+    local lastColor = reset.color
+
+    local function removeLastSpaceAndAdd(text, color, newline)
+        if #text > 0 and text:sub(#text) == " " then
+            text = text:sub(1, #text - 1)
+        end
+        if #text > 0 or newline then
+            table.insert(str, { text = text, color = color, newline = newline })
+        end
+        lastColor = color
+    end
+
+    for _, p in ipairs(parts) do
+        local char = p:sub(1, 1)
+        local text = p:sub(2)
+        if text:sub(1, 1) == " " then
+            -- if first char after code is space
+            text = text:sub(2)
+        end
+        if char == newlineChar then
+            -- newline
+            if key ~= "Name" then
+                removeLastSpaceAndAdd(text, lastColor, true)
+            end
+        elseif char == reset.char then
+            -- char is reset
+            removeLastSpaceAndAdd(text, reset.color)
+        else
+            local newColor
+            for column = 2, #FORMATTING_CODES do
+                for _, col in ipairs(FORMATTING_CODES[column]) do
+                    local colChar = col.code:sub(2)
+                    if colChar == char then
+                        newColor = col.color
+                        break
+                    end
+                end
+                if newColor then
+                    break
+                end
+            end
+            if newColor then
+                -- char is a new color
+                removeLastSpaceAndAdd(text, newColor)
+            else
+                -- char is not a color
+                removeLastSpaceAndAdd(text, lastColor)
+            end
+        end
+    end
+    if key == "Name" then
+        table.insert(str, { text = FAVORITE_NAME_OFFLINE_SUFFIX, color = lastColor })
+    end
+
+    cols:addRow({
+        cells = {
+            function()
+                LineBuilder()
+                    :helpMarker(BJILang.get("serverConfig.core.previewTooltip"))
+                    :build()
+            end,
+            function()
+                local line = LineBuilder()
+                for _, s in ipairs(str) do
+                    if s.newline then
+                        line:build()
+                        line = LineBuilder()
+                    end
+                    line:text(s.text, s.color)
+                end
+                line:build()
+            end
+        }
+    })
+end
+
 local function drawCoreConfig(ctxt)
     LineBuilder()
         :text(svar("{1}:", { BJILang.get("serverConfig.core.title") }))
         :build()
+
+    Indent(2)
+    drawCoreFormattingHints()
 
     local labelWidth = 0
     for k in pairs(BJIContext.Core) do
@@ -11,7 +162,6 @@ local function drawCoreConfig(ctxt)
             labelWidth = w
         end
     end
-    Indent(2)
     local cols = ColumnsBuilder("CoreSettings", { labelWidth, -1 })
     for k, v in pairs(BJIContext.Core) do
         cols:addRow({
@@ -74,6 +224,26 @@ local function drawCoreConfig(ctxt)
                 end
             }
         })
+
+        if tincludes({ "Name", "Description" }, k) then
+            if v:find("%^") then
+                local colorFound = false
+                for i = 2, #FORMATTING_CODES do
+                    for _, color in ipairs(FORMATTING_CODES[i]) do
+                        if v:find(svar("%{1}", { color.code })) then
+                            colorFound = true
+                            break
+                        end
+                    end
+                    if colorFound then
+                        break
+                    end
+                end
+                if colorFound then
+                    drawCoreTextPreview(cols, v:gsub("\n", "%^p"), k)
+                end
+            end
+        end
     end
     cols:build()
     Indent(-2)
