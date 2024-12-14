1966ba89ffcb9a1dfdb74c3afcc282c04cd3715f
my-name-is-samael <warkramzec@gmail.com>
2024-12-15 00:50:16 +0100

    1.1.4 + Fixed default environment values

--- BeamJoyInterface/lua/ge/extensions/BJI.lua
+++ BeamJoyInterface/lua/ge/extensions/BJI.lua
@@ -18,7 +18,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 Contact : https://github.com/my-name-is-samael
 ]]
 
-BJIVERSION = "1.1.3"
+BJIVERSION = "1.1.4"
 
 local managers = {}
 function GetBJIManagers()
