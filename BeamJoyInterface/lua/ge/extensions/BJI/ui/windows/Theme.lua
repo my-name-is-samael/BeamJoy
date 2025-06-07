b1e0e05be2b881b2bf9f455c5638765b46be1f74
my-name-is-samael <warkramzec@gmail.com>
2025-06-07 04:57:16 +0200

    fixed theme btns sync after save

--- BeamJoyInterface/lua/ge/extensions/BJI/ui/windows/Theme.lua
+++ BeamJoyInterface/lua/ge/extensions/BJI/ui/windows/Theme.lua
@@ -6,6 +6,7 @@ local W = {
 
     show = false,
     changed = false,
+    saveProcess = false,
     ---@type table<string, table>?
     data = nil,
     labels = {
@@ -101,6 +102,15 @@ local function onLoad()
     listeners:insert(BJI.Managers.Events.addListener({
         BJI.Managers.Events.EVENTS.UI_SCALE_CHANGED,
     }, updateWidths, W.name))
+
+    listeners:insert(BJI.Managers.Events.addListener(BJI.Managers.Events.EVENTS.CACHE_LOADED,
+        function(_, data)
+            if W.saveProcess and data.cache == BJI.Managers.Cache.CACHES.BJC then
+                W.open()
+                W.changed = false
+                W.saveProcess = false
+            end
+        end, W.name))
 end
 local function onUnload()
     listeners:forEach(BJI.Managers.Events.removeListener)
@@ -114,6 +124,7 @@ local function updateTheme()
 end
 
 local function save()
+    W.saveProcess = true
     BJI.Tx.config.bjc("Server.Theme", W.data)
 end
 
