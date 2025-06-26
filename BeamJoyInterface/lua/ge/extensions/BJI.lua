88a5be7f534dab6653abf09d53bd57d73dd9fed3
my-name-is-samael <warkramzec@gmail.com>
2025-06-26 13:28:47 +0200

    1.1.8 version

--- BeamJoyInterface/lua/ge/extensions/BJI.lua
+++ BeamJoyInterface/lua/ge/extensions/BJI.lua
@@ -18,7 +18,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 Contact : https://github.com/my-name-is-samael
 ]]
 
-BJIVERSION = "1.1.6"
+BJIVERSION = "1.1.8"
 
 require("ge/extensions/utils/LoadDefaults")
 
