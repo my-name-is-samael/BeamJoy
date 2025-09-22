bbdd77956606ddf41fe53e3f64aa2841dcbb6cc9
my-name-is-samael <warkramzec@gmail.com>
2025-09-22 16:11:04 +0200

    v2.0.6

--- BeamJoyInterface/lua/ge/extensions/BeamJoyInterface.lua
+++ BeamJoyInterface/lua/ge/extensions/BeamJoyInterface.lua
@@ -27,7 +27,7 @@ require("ge/extensions/utils/String")
 require("ge/extensions/utils/Table")
 
 BJI = {
-    VERSION = "2.0.5",
+    VERSION = "2.0.6",
     tag = "BJI",
     CLIENT_READY = false,
     CONSTANTS = require("ge/extensions/utils/Constants"),
